f2ba72241342fabed8ea0a743dd7304e50a4763f
etan8225 <eoconnor8225@gmail.com>
2019-01-11 23:03:48 -0500

    Delete problemSolver.cpp

--- problemSolver.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-void problemSolver(int x, int y, int b)
-{
-    x = -x;
-    std::cout << y << "y = " << x << "x + " << b << std::endl;
-    std::cout << "The slope is: " << x << std::endl;
-    std::cout << "The y-intercept is: " << b << std::endl;
-}
