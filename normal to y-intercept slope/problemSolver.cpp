570815a67acf07e6320259f06e8a51ccbabb1528
etan8225 <eoconnor8225@gmail.com>
2019-01-13 14:37:48 -0500

    Update problemSolver.cpp

--- normal to y-intercept slope/problemSolver.cpp
+++ normal to y-intercept slope/problemSolver.cpp
@@ -4,6 +4,6 @@ void problemSolver(int x, int y, int b)
 {
     x = -x;
     std::cout << y << "y = " << x << "x + " << b << std::endl;
-    std::cout << "The slope is: " << x << std::endl;
-    std::cout << "The y-intercept is: " << b << std::endl;
+    std::cout << "The slope is: " << x << " divided by " << y << std::endl;
+    std::cout << "The y-intercept is: " << b << " divided by " << y << std::endl;
 }
