6427624804cf803caf8ef74cf26ed599b92dcee5
etan8225 <eoconnor8225@gmail.com>
2019-01-15 21:31:29 -0500

    Update headers.h

--- normal to y-intercept slope/headers.h
+++ normal to y-intercept slope/headers.h
@@ -1,6 +1,9 @@
 #ifndef questionAsker1_2_3_h
 #define questionAsker1_2_3_h
 
+#include "numberOrganizer.cpp"
+#include "questionAskers.cpp"
+
 void numberOrganizer(long double x, long double b, long double y);
 long double question1();
 long double question2();
